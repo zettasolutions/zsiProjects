97b44d321013f5fc0b4bb00b18cf921f604e0b0f
rdizon <rdizon83@gmail.com>
2016-08-24 20:31:22 +0800

    created tables

--- inventory/Web/inventory/dbWebSource/js/unitconversion.js
+++ inventory/Web/inventory/dbWebSource/js/unitconversion.js
@@ -1,20 +1,16 @@
-  var bs = zsi.bs.ctrl;
+var bs = zsi.bs.ctrl;
 var svn =  zsi.setValIfNull;
 
 
 zsi.ready(function(){
   
-   /* $(".zPanel").css({
-            height:$(window).height()-179
-        });*/
-     
-    displayRecords();
+   displayRecords();
   
 });
 
 $("#btnSave").click(function () {
     $("#grid").jsonSubmit({
-              procedure  : "units_upd"
+              procedure  : "conv_units_upd"
              ,onComplete : function (data) {
                   $("#grid").clearGrid();
                   if(data.isSuccess===true) zsi.form.showAlert("alert");
@@ -28,8 +24,8 @@ $("#btnSave").click(function () {
  function displayRecords(){
       var cb = bs({name:"cbFilter1",type:"checkbox"});
      $("#grid").dataBind({
-	     url            : execURL + "units_sel"
-	    ,width          : 435
+	     url            : execURL + "conv_units_sel"
+	    ,width          : 490
 	    ,height         : 506
 	    ,selectorType   : "checkbox"
         ,blankRowsLimit:5
@@ -37,26 +33,29 @@ $("#btnSave").click(function () {
     		
             	 {text  : cb                                 , width : 25        , style : "text-align:left;"       
             		    , onRender      :  function(d){ 
-                		              return bs({name:"unit_id"   ,value: svn (d,"unit_id")    ,type:"hidden"})
+                		              return bs({name:"conv_id"   ,value: svn (d,"conv_id")    ,type:"hidden"})
                 		                                +  (d !==null ? bs({name:"cb",type:"checkbox"}) : "" );
                     }
                 }	 
-            	,{ text:"Code"                           , width:200        , style:"text-align:center;"        , type:"input"          ,name:"unit_sdesc"}
-                ,{ text:"Description"                    , width:200        , style:"text-align:center;"        , type:"input"          ,name:"unit_desc"}
+            	,{ text:"Convert From"     , width:150        , style:"text-align:center;"        , type:"select"          ,name:"from_unit_id"}
+            	,{ text:"Convert To"       , width:150        , style:"text-align:center;"        , type:"select"          ,name:"conv_unit_id"}
+                ,{ text:"Convert Qty"      , width:150        , style:"text-align:center;"        , type:"input"           ,name:"conv_unit_qty"}
 	    ]
 	    ,onComplete: function(){
             $("#cbFilter1").setCheckEvent("#grid input[name='cb']");
+            $("select[name='from_unit_id']").dataBind( "units");
+            $("select[name='conv_unit_id']").dataBind( "units");
 	    }
     });    
 }
 
 $("#btnDelete").click(function(){
     zsi.form.deleteData({
-         code       : "ref-0005"
+         code       : "ref-0006"
         ,onComplete : function(data){
                         displayRecords();
                       }
     });   
 
 });   
-          
+           
