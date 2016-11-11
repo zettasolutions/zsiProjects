77ccfec7798ae73cfe9a050e0efcb1e98c40dde6
rdizon <rdizon83@gmail.com>
2016-11-12 01:24:19 +0800

    updated warehouse issuance

--- pafeis/dbWebSource/js/warehouseIssuance.js
+++ pafeis/dbWebSource/js/warehouseIssuance.js
@@ -1,38 +1,52 @@
 var  bs = zsi.bs.ctrl
     ,svn =  zsi.setValIfNull
-    ,tblID = "tblIssuanceDetails"
 ;
 
 
 
 zsi.ready(function(){
-    getTemplate();
     displayRecords();
+    displayRecordsIssuanceDetails();
+    $("select[name='authority_id']").dataBind("wing");
+    
+    $("select[name='authority_id']").change(function(){  
+        if(this.value)
+            $("select[name='issued_to_id']").dataBind("squadron @wing_id=" + this.value);
+        else
+            alert("Please select authority.");
+    });
+    
+   // $("select[name='issued_by']").dataBind("wing");
+    
+    setCurrentTab();
 });
 
-      var contextIssuanceDetails = { id:"modalWindowIssuanceDetails"
-                        , title: "Item Types"
-                        , sizeAttr: "modal-lg"
-                        , footer: '<div class="pull-left"><button type="button" onclick="submitItemsTypes();" class="btn btn-primary">'
-                        + '<span class="glyphicon glyphicon-floppy-disk"></span>&nbsp;Save</button>'
-                                + '<button type="button" onclick="manageInactiveItemTypes();" class="btn btn-primary"><span class="glyphicon glyphicon-ban-circle">'
-                                + '</span>&nbsp;Inactive</button>'
+function setCurrentTab(){
+    var projectTabIndex = readCookie("project_tab_index");
+    if(typeof projectTabIndex === ud) projectTabIndex=0;
+    var $tabs = $("#tabPanel > div");
+    var $navTabs = $("ul.nav-tabs > li");
+    $tabs.removeClass("active");
+    $navTabs.removeClass("active");
+    $($tabs.get(projectTabIndex)).addClass("active");
+    $($navTabs.get(projectTabIndex)).addClass("active");
+}
 
-                        , body:            
-                        '<div id="' + tblID + '" class="zGrid"></div>'
-                             
-        };
+$("ul.nav-tabs >li").click(function(){
+     
+    var cur_li_Index = $(this).index();
+    
+    createCookie("project_tab_index",cur_li_Index,1);
+    $("#tabPanel > div").each(function(){
+        var obj =  $(this);
+        var cur_div_index = obj.index();
+        obj.removeClass("active");
+        if(cur_li_Index===cur_div_index)
+           obj.addClass("active");
+    });
+ });
 
 
-function getTemplate(callback){
-    $.get(base_url + "templates/bsDialogBox.txt",function(d){
-        var template = Handlebars.compile(d); 
-        
-        $("body").append(template(contextIssuanceDetails));
-
-        if(callback) callback();
-    });    
-}  
 $("#btnSave").click(function () {
    $("#grid").jsonSubmit({
              procedure: "item_categories_upd"
@@ -45,79 +59,42 @@ $("#btnSave").click(function () {
     });
 });
 
-function submitItemsTypes(){
-          $("#frm_modalWindowItemTypes").jsonSubmit({
-             procedure      : "item_types_upd"
-            ,optionalItems  : ["item_cat_id","monitoring_type_id","is_active"]
-            ,onComplete     : function (data) {
-               if(data.isSuccess===true) zsi.form.showAlert("alert");
-               $("#grid").trigger("refresh");
-               displayRecordsItemTypes(item_cat_id);
-                
-            }
-        });
-}
+$("#issued_date").datepicker()
+    .on("show", function(e) {
+        issuedDate = ($(this).val()? $(this).val():"");
+    }).on("changeDate", function(e) {
+        issuedDate = $(this).val();
+    }).on("hide", function(e) {
+        $(this).val(issuedDate);
+});
 
-function submitItemsCodes(){
-          $("#frm_modalWindowItemCodes").jsonSubmit({
-             procedure      : "item_codes_upd"
-            ,optionalItems  : ["item_type_id","is_active"]
-            ,onComplete     : function (data) {
-               if(data.isSuccess===true) zsi.form.showAlert("alert");
-               displayRecordsItemTypes(item_cat_id);
-               displayRecordsItemCodes(item_type_id);
-                
-            }
-        });
-}
-
-function manageIssuanceDetails(id,title){
-        issuance_id=id;
-        displayRecordsIssuanceDetails(id);
-        $("#frm_modalWindowItemTypes .modal-title").text("Item Types for » " + title);
-        $('#modalWindowItemTypes').modal({ show: true, keyboard: false, backdrop: 'static' });
-        $('#modalWindowItemTypes');//.setCloseModalConfirm();
-        $("#modalWindowItemTypes .modal-body").css("height","450px");
-}
+function displayRecords(){ 
     
+    $.get( execURL + "issuances_sel " 
+    ,function(data){
+       // var i = data.rows[0];
+        if(data.rows.lenght > 0){
+            $("#issuance_no").val(  data.rows[0].issuance_no );
+            $("#authority_id").val(  data.rows[0].authority_id );
+            $("#issued_to_id").val(  data.rows[0].issued_to_id );
+            $("#issued_by").val(  data.rows[0].issued_by );
+            $("#issued_date").val(  data.rows[0].issued_date.toDateFormat() );
+            $("#issuance_directive_id").val(  data.rows[0].issuance_directive_id );
+            $("#page_process_action_id").val(  data.rows[0].page_process_action_id );
+            $("#remarks").val(  data.rows[0].remarks );
 
-function displayRecords(){
-     var cb = bs({name:"cbFilter1",type:"checkbox"});
-     $("#grid").dataBind({
-	     url            : execURL + "issuances_sel"
-	    ,width          : $(document).width() - 35
-	    ,height         : $(document).height() - 250
-	    ,selectorType   : "checkbox"
-        ,blankRowsLimit:5
-        ,isPaging : false
-        ,dataRows : [
-                 {text  : cb                                                           , width : 25        , style : "text-align:left;"       
-        		    , onRender      :  function(d){ 
-                		                    return     bs({name:"issuance_id",type:"hidden",value: svn (d,"issuance_id")})
-                                                      +  (d !==null ? bs({name:"cb",type:"checkbox"}) : "" );
-                            }
-            }	 
-        		,{text  : "Issuance No."          , name  : "issuance_no"          , type  : "input"             , width : 50        , style : "text-align:left;"}
-        		,{text  : "Authority"             , name  : "authority_id"         , type  : "select"            , width : 200       , style : "text-align:left;"}
-        		,{text  : "Issued to"             , name  : "issued_to_id"         , type  : "select"            , width:55          , style : "text-align:left;"   ,defaultValue:"Y"}
-        		,{text  : "Issuance Details"          , width : 80                 , style : "text-align:center;"      
-                    ,onRender  :  
-                        function(d){return "<a href='javascript:manageIssuanceDetails(" + svn(d,"issuance_id") + ",\"" +  svn(d,"issuance_no")  + "\");'>" + svn(d,"countIssuanceDetails") + "</a>"; 
-                    }
-                }
+        }
 
-	    ] 
-    	     ,onComplete: function(){
-                $("#cbFilter1").setCheckEvent("#grid input[name='cb']");
-        }  
-    });    
+    });
+
 }
 
-function displayRecordsIssuanceDetails(id){
+
+function displayRecordsIssuanceDetails(){
      var cb = bs({name:"cbFilter2",type:"checkbox"});
-     $("#" + tblID).dataBind({
-	     url            : execURL + "issuance_details_sel @issuance_id=" + id
-	    ,width          : 750
+     $("#grid").dataBind({
+	     url            : execURL + "issuance_details_sel " 
+	    ,width          : $(document).width() - 120
 	    ,height         : 400
 	    ,selectorType   : "checkbox"
         ,blankRowsLimit : 5
@@ -126,47 +103,20 @@ function displayRecordsIssuanceDetails(id){
                  {text  : cb     , width : 25        , style : "text-align:left;"       
         		    , onRender      :  function(d){ 
                 		                    return     bs({name:"issuance_detail_id",type:"hidden",value: svn (d,"issuance_detail_id")})
-                		                            +  bs({name:"issuance_id",type:"hidden",value: id})
+                		                            +  bs({name:"issuance_id",type:"hidden",value: issuance_id})
                                                     +  (d !==null ? bs({name:"cb",type:"checkbox"}) : "" );
                             }
             }	 
-        		,{text  : "Item"                , name  : "item_id"                 , type  : "select"       , width : 160       , style : "text-align:left;"}
+        		,{text  : "Item"                , name  : "item_id"                 , type  : "select"       , width : 260       , style : "text-align:left;"}
         		,{text  : "Aircraft"            , name  : "aircraft_id"             , type  : "select"       , width : 200       , style : "text-align:left;"}
-        		,{text  : "Unit of Measurement" , name  : "unit_of_measure_id"      , type  : "select"       , width : 200       , style : "text-align:left;"}
-        		,{text  : "Quantiy"             , name  : "quantity"                , type  : "yesno"        , width : 75        , style : "text-align:center;"  ,defaultValue:"Y"}
+        		,{text  : "Unit of Measurement" , name  : "unit_of_measure_id"      , type  : "select"       , width : 350       , style : "text-align:left;"}
+        		,{text  : "Quantity"            , name  : "quantity"                , type  : "input"        , width : 75        , style : "text-align:center;" }
+        		,{text  : "Remarks"             , name  : "remarks"                 , type  : "input"        , width : 450       , style : "text-align:center;" }
 
 	    ] 
     	     ,onComplete: function(){
-                $("#cbFilter2").setCheckEvent("#" + tblID + " input[name='cb']");
-                $("select[name='monitoring_type_id']").dataBind( "monitoring_type");
-        }  
-    });    
-}    
-
-function displayRecordsItemCodes(id){
-     var cb = bs({name:"cbFilter3",type:"checkbox"});
-     $("#" + tblIC).dataBind({
-	     url            : execURL + "item_codes_sel @item_type_id=" + id
-	    ,width          : 750
-	    ,height         : 400
-	    ,selectorType   : "checkbox"
-        ,blankRowsLimit : 5
-        ,dataRows       : [
-            
-                 {text  : cb     , width : 25        , style : "text-align:left;"       
-        		    , onRender      :  function(d){ 
-                		                    return     bs({name:"item_code_id",type:"hidden",value: svn (d,"item_code_id")})
-                		                            +  bs({name:"item_type_id",type:"hidden",value: id})
-                                                    +  (d !==null ? bs({name:"cb",type:"checkbox"}) : "" );
-                            }
-            }	 
-        		,{text  : "Part No."            , name  : "part_no"               , type  : "input"         , width : 65       , style : "text-align:left;"}
-        		,{text  : "National Stock No."  , name  : "national_stock_no"     , type  : "input"         , width : 150       , style : "text-align:left;"}
-        		,{text  : "Item Name"           , name  : "item_name"             , type  : "input"         , width : 400      , style : "text-align:left;"}
-        		,{text  : "Is Active?"          , width : 100                      , type  : "yesno"         , style : "text-align:center;"  ,defaultValue:"Y"}
-	    ] 
-    	     ,onComplete: function(){
-                $("#cbFilter3").setCheckEvent("#" + tblIC + " input[name='cb']");
+                $("#cbFilter2").setCheckEvent("#grid input[name='cb']");
+                $("select[name='item_id']").dataBind("squadron");
         }  
     });    
 }    
@@ -174,11 +124,11 @@ function displayRecordsItemCodes(id){
 
 $("#btnDelete").click(function(){
     zsi.form.deleteData({
-         code       : "ref-0005"
+         code       : "ref-0006"
         ,onComplete : function(data){
                         displayRecords();
                       }
     });       
 });
     
-                                              
+                                                  
