31387ef267e5eaefa1d832cfbbf5cc40181093c9
rdizon <rdizon83@gmail.com>
2017-06-01 00:47:08 +0800

    change filter to dropdown and add columns to report list

--- pafeis/dbWebSource/js/CriticalStockpileReport.js
+++ pafeis/dbWebSource/js/CriticalStockpileReport.js
@@ -1,32 +1,36 @@
- var bs = zsi.bs.ctrl;
-var svn =  zsi.setValIfNull;
+var bs  = zsi.bs.ctrl;
+var svn = zsi.setValIfNull;
 
 zsi.ready(function(){
     displayRecords();
 });
+
+$("select[name='criticalstockpilereport_filter']").dataBind({
+    url: procURL + "dd_warehouse_organizations_sel" 
+    , text : "organization_warehouse"
+    , value: "warehouse_id"
+});
+
 $("#btnGo").click(function(){
-   displayRecords($("#criticalstockpilereport_filter").val());
+   displayRecords();
 });
 
 function displayRecords(){
-     var cb = bs({name:"cbFilter1",type:"checkbox"});
-     $("#grid").dataBind({
-	     url            : execURL + "critical_stockpile_report_sel"
-	    ,width          : $(document).width() - 35
-	    ,height         : $(document).height() - 250
-	    ,selectorType   : "checkbox"
-        //,blankRowsLimit:5
-        ,isPaging : false
-        ,dataRows : [
-                 {text  : "Unit"                , name  :"organization_code"    , width : 150           , style : "text-align:left;"}
-        		,{text  : "Part No."            , name  :"part_no"              , width : 150           , style : "text-align:left;"}
-        		,{text  : "National Stock No."  , name  :"national_stock_no"    , width : 150           , style : "text-align:left;"}
-        		,{text  : "Nomenclature"        , name  : "item_name"           , width : 300           , style : "text-align:left"}
-        	    ,{text  : "Available Stocks"    , name  : "available_stocks"    , width : 130           , style : "text-align:center;"}
-	    ]   
-    	     ,onComplete: function(){
-              
-        }  
+    var wId = $("#criticalstockpilereport_filter").val();
+    console.log(wId);
+        $("#grid").dataBind({
+             url            : execURL + "critical_stockpile_report_sel @warehouse_id=" + (wId ? wId : null)
+            ,width          : $(document).width() - 35
+            ,height         : $(document).height() - 250
+            ,isPaging : false
+            ,dataRows : [
+            		 {text  : "Part No."            , name  :"part_no"              , width : 150           , style : "text-align:left;"}
+            		,{text  : "National Stock No."  , name  :"national_stock_no"    , width : 150           , style : "text-align:left;"}
+            		,{text  : "Nomenclature"        , name  : "item_name"           , width : 450           , style : "text-align:left"}
+            	    ,{text  : "Stock Qty."          , name  : "stock_qty"           , width : 130           , style : "text-align:center;"}
+            	    ,{text  : "Unit of Measure."    , name  : "unit_of_measure"     , width : 130           , style : "text-align:center;"}
+            	    ,{text  : "Reoder Level"        , name  : "reorder_level"       , width : 130           , style : "text-align:center;"}
+            ]   
     });    
 }
-                             
+                              
