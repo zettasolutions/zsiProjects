e781f38b500f08898f95dbff769357cacbaf6ce8
rdizon <rdizon83@gmail.com>
2017-06-02 03:43:21 +0800

    added inActive Modal

--- pafeis/dbWebSource/js/MonitoringTypes.js
+++ pafeis/dbWebSource/js/MonitoringTypes.js
@@ -1,12 +1,50 @@
-  var bs = zsi.bs.ctrl;
-var svn =  zsi.setValIfNull;
-
-
+var bs = zsi.bs.ctrl
+    ,svn =  zsi.setValIfNull
+;
 
 zsi.ready(function(){
     displayRecords();
+    getTemplate();
 });
 
+function getTemplate(){
+    $.get(base_url + "templates/bsDialogBox.txt",function(d){
+        var template = Handlebars.compile(d);     
+
+        var context = { id:"modalWindow"
+                        , sizeAttr: "modal-xs"
+                        , title: "Monitoring Types"
+                        , footer:  ' <div class="pull-left"><button type="button" onclick="submitItems();" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> Save</button>'
+                                +  ' <button type="button" onclick="deleteItems();" class="btn btn-primary"><span class="glyphicon glyphicon-trash"></span> Delete</button></div>'
+                        , body: '<div id="inActiveRecords" class="zGrid" ></div>'
+                      };
+        var html    = template(context);     
+        $("body").append(html);
+    });    
+}
+
+function deleteItems(){
+    zsi.form.deleteData({
+         code       : "ref-0004"
+        ,onComplete : function(data){
+                $("#grid").trigger('refresh');
+                displayInactive();
+        }
+    }); 
+}      
+
+function submitItems(){    
+  $("#frm_modalWindow").jsonSubmit({
+            procedure  : "monitoring_types_upd"
+            ,optionalItems: ["is_active"]
+            //,notInclude: "#employee_name"
+            ,onComplete : function (data) {
+                 if(data.isSuccess===true) zsi.form.showAlert("alert");
+                 $("#grid").trigger('refresh');
+                 displayInactive();
+            }
+    });        
+}
 
 $("#btnSave").click(function () {
   //  console.log("test");
@@ -20,6 +58,13 @@ $("#btnSave").click(function () {
             }
     });
 });
+  
+$("#btnInactive").click(function () {
+    $(".modal-title").text("Inactive Monitoring Type");
+    $('#modalWindow').modal({ show: true, keyboard: false, backdrop: 'static' });
+    //$('#modalWindow').setCloseModalConfirm(); 
+    displayInactive();
+}); 
     
 function displayRecords(){
      var cb = bs({name:"cbFilter1",type:"checkbox"});
@@ -47,14 +92,32 @@ function displayRecords(){
     });    
 }
     
+function displayInactive(){
+     var cb = bs({name:"cbFilter2",type:"checkbox"});
+     $("#inActiveRecords").dataBind({
+	     url            : execURL + "monitoring_types_sel @is_active='N'" 
+	    ,width          : 550
+	    ,height         : $(document).height() - 250
+	    ,selectorType   : "checkbox"
+        ,blankRowsLimit:5
+        ,isPaging : false
+        ,dataRows : [
+                 {text  : cb                                                           , width : 25        , style : "text-align:left;"       
+        		    , onRender      :  function(d){ 
+                		                    return     bs({name:"monitoring_type_id",type:"hidden",value: svn (d,"monitoring_type_id")})
+                                                      +  (d !==null ? bs({name:"cb",type:"checkbox"}) : "" );
+                            }
+            }	 
+        		,{text  : "Code"                , name  : "monitoring_type_code"        , type  : "input"         , width : 85       , style : "text-align:left;"}
+        		,{text  : "Name"                , name  : "monitoring_type_name"        , type  : "input"         , width : 300      , style : "text-align:left;"}
+        		,{text  : "Active?"             , name  : "is_active"                   , type  : "yesno"         , width : 75       , style : "text-align:left;"   ,defaultValue:"Y"                 }
+	    ] 
+    	     ,onComplete: function(){
+                $("#cbFilter2").setCheckEvent("#inActiveRecords input[name='cb']");
+        }  
+    });    
+}
 
-$("#btnDelete").click(function(){
-    zsi.form.deleteData({
-         code       : "ref-0004"
-        ,onComplete : function(data){
-                        displayRecords();
-                      }
-    });       
-});
+
     
-                                            
+                                             
