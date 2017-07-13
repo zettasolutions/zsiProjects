386d713efb34825f4d8cadc43d0c19d3b55f1c5a
hiplord mahilum <hlmahilum@gmail.com>
2017-07-13 16:17:39 +0800

    AdjustmentTypes.html, CriticalStockpileReport.js, docs.html, AdjustmentTypes.html

--- pafeis/dbWebSource/js/CriticalStockpileReport.js
+++ pafeis/dbWebSource/js/CriticalStockpileReport.js
@@ -46,6 +46,9 @@ $("#btnGo").click(function(){
 
 });
 
+
+
+
 function displayRecords(){
     var wId = $("#warehouse_filter").val();
     var catId = $("#categories_filter").val();
@@ -114,8 +117,15 @@ $("#btnPdf").click(function(){
             o.doc.text(o.margin.left, o.row, "Critical Stock Pile Report");
             o.doc.setFontSize(10);
             o.row +=16;
+            
+            o.doc.setFontSize(12);
+            o.doc.text(o.margin.left, o.row, "Warehouse: ");
+            o.doc.setFontSize(10);
+            o.row +=16;
+            
             return o;
         }
+        
        
         
     });         
@@ -123,4 +133,85 @@ $("#btnPdf").click(function(){
     
     
 });
-                                   
+/*
+$("#btnPdf").click(function(){
+    var mw = $('#modalWindow');
+    mw.modal({ show: true, keyboard: false, backdrop: 'static' });
+    mw.find(".modal-title").text("Procurement Report");
+    
+
+    zsi.createPdfReport({
+         margin             : { top :30  ,left:25 }
+        ,cellMargin         : { left: 5 }
+        ,isDisplay          : true
+        ,fileName           : "criticalstockpile.pdf"  
+        ,rowHeight          : 14
+        ,widthLimit         : 520
+        ,pageHeightLimit    : 800
+      //  ,masterKey          : "procurement_id"
+        ,columnHeader       :  [   
+                                 {name:"part_no"                ,title:"Part No."                           ,titleWidth:100 ,width:120}
+                                ,{name:"national_stock_no"      ,title:"National Stock No."                 ,titleWidth:100 ,width:120}
+                                ,{name:"item_name"              ,title:"Nomenclature"                       ,titleWidth:100 ,width:300}
+                                ,{name:"stock_qty"              ,title:"Stock Qty."                         ,titleWidth:100 ,width:70}
+                                ,{name:"unit_of_measure"        ,title:"Unit of Measure."                   ,titleWidth:100 ,width:90}
+                                ,{name:"reorder_level"          ,title:"Reoder Level"                       ,titleWidth:100 ,width:90}
+                            ]
+                            
+        ,masterData         : g_masterData
+//,detailData         : g_detailData
+        ,onPrintHeader      : function(o){
+            if (g_imgData) {
+                o.doc.addImage(g_imgData, 'JPEG', o.margin.left,  o.row, 50, 50);
+            }
+            o.row +=27;
+            o.doc.setFontSize(12);
+            o.doc.text(o.margin.left + 60, o.row, "Philippine Airforce");
+            o.row +=40;
+            o.doc.setFontSize(14);
+            o.doc.text(o.margin.left, o.row, "Critical Stock Pile Report");
+            o.doc.setFontSize(10);
+            o.row +=16;
+            return o;
+        }
+        //customized master data printing
+        
+       /* ,onMasterDataPrint : function(o){
+            if(o.index>0) o.row +=14; 
+            //setBoxColor(o.doc,207, 226, 247);
+            //o.doc.rect(20, o.row-10, 90,14, 'FD');    
+            o.doc.text(25, o.row, "Warehouse:");
+            
+            //setBoxColor(o.doc,255, 255, 255);
+            //o.doc.rect(120, o.row-10, 60,14, 'FD');    
+            o.doc.text(125, o.row, ": "  + o.data.organization_warehouse);
+
+/*
+            //setBoxColor(o.doc,207, 226, 247);
+            //o.doc.rect(200, o.row-10, 100,14, 'FD');    
+            o.doc.text(205, o.row, "Procurement Name");
+            
+            //setBoxColor(o.doc,255, 255, 255);
+            //o.doc.rect(310, o.row-10, 110,14, 'FD');    
+            o.doc.text(315, o.row,  ": "  + o.data.procurement_name);
+            
+            //new row
+            o.row +=18; 
+            //setBoxColor(o.doc,207, 226, 247);
+            //o.doc.rect(20, o.row-10, 90,14, 'FD');    
+            o.doc.text(25, o.row, "Supplier");
+            
+            //setBoxColor(o.doc,255, 255, 255);
+            //o.doc.rect(120, o.row-10, 60,14, 'FD');    
+            o.doc.text(125, o.row, ": "  + o.data.supplier_name);
+            
+            return o.row;    
+        }  
+        
+        
+    });         
+    
+    
+    
+});*/
+                                     
