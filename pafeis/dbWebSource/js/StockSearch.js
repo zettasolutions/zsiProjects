6c902f37988c5d7162c98f70c4d540e94a546e29
rdizon <rdizon83@gmail.com>
2017-08-03 23:36:00 +0800

    remove status filter and added 3 columns to grid

--- pafeis/dbWebSource/js/StockSearch.js
+++ pafeis/dbWebSource/js/StockSearch.js
@@ -6,21 +6,20 @@ var wingFilter = null
     ,statusFilter = null
     ,fieldFilter = ""
     ,keywordFilter = ""
-    ,g_organization_id = null;
+    ,g_organization_id = null
+    ,g_squadron_id = null;
     
 zsi.ready(function(){
     setInputs();
     loadWing();
     loadSquadron();
     loadWarehouse();
-    loadStatus();
 });
 
 function setInputs(){
     $wingFilter = $("#wing_filter");
     $squadronFilter = $("#squadron_filter");
     $warehouseFilter = $("#warehouse_filter");
-    $statusFilter = $("#status_filter");
     $fieldFilter = $("#field_filter");
     $keywordFilter = $("#keyword_filter");
 }
@@ -31,6 +30,7 @@ function loadWing(){
         , text: "organization_name"
         , value: "organization_id"
         , onComplete: function(){
+            $wingFilter.unbind();
             $wingFilter.change(function(){
                 g_organization_id = (this.value !==""? this.value: null);
                 loadSquadron();
@@ -44,22 +44,21 @@ function loadSquadron(){
         url: procURL + "dd_organizations_sel @organization_id="+ g_organization_id +",@squadron_type='Supply'" 
         , text: "organization_name"
         , value: "organization_id"
+        , onComplete: function(){
+            $squadronFilter.unbind();
+            $squadronFilter.change(function(){
+                g_squadron_id = (this.value !==""? this.value: null);
+                loadWarehouse();
+            });
+        }
     });
 }
 
 function loadWarehouse(){
     $warehouseFilter.dataBind({
-       url: procURL + "dd_warehouses_sel"
+       url: procURL + "dd_warehouses_sel @squadron_id="+ g_squadron_id
        , text: "warehouse"
        , value: "warehouse_id"
-    });
-}
-
-function loadStatus(){
-    $statusFilter.dataBind({
-        url: execURL + "statuses_sel @is_item='Y'"
-        ,text: "status_name"
-        ,value: "status_id"
     });
 }
 
@@ -71,7 +70,6 @@ function setSearch(){
         ,searchColumn :"serial_no"
         ,input:"input[name=stock_search_filter]"
         ,url : execURL + "searchData"
-        //,condition :"'is_active=''Y'''"
         ,onSelectedItem: function(currentObject,data,i){ 
             currentObject.value=data.serial_no;
             var tr  = currentObject.parentNode.parentNode;
@@ -84,7 +82,6 @@ function setSearch(){
 $("#btnGo").click(function(){
     squadronFilter = ($squadronFilter.val()!==""? $squadronFilter.val() : null);
     warehouseFilter = ($warehouseFilter.val()!==""? $warehouseFilter.val() : null);
-    statusFilter = ($statusFilter.val()!==""? $statusFilter.val() : null);
     fieldFilter = $fieldFilter.val();
     keywordFilter = $.trim($keywordFilter.val());
     displayRecords();
@@ -95,17 +92,14 @@ function clearGrid(){
 }
     
 function displayRecords(){   
-    //console.log(filter);
     var rownum=0;
     if(keywordFilter==="")
     {
         $("#grid").dataBind({
-	     url   : execURL + "stock_monitoring_sel @squadron_id="+ squadronFilter +",@aircraft_type_id="+ typeFilter +",@status_id="+ statusFilter +",@field='"+ fieldFilter +"', @search='"+keywordFilter+"'" 
+	     url   : execURL + "stock_monitoring_sel @field='"+ fieldFilter +"', @search='"+keywordFilter+"'" 
         ,width          : $(document).width() - 25
 	    ,height         : $(document).height() - 360
-	   // ,selectorType   : "checkbox"
         ,blankRowsLimit :0
-       // ,isPaging : true
         ,dataRows       :[
      		 { text:"Result"                      , width:$(document).width() - 56     , style:"text-align:center;"        
      		    ,onRender: function(d){
@@ -115,55 +109,61 @@ function displayRecords(){
      		 ]});
     }
     else{
-    $("#grid").dataBind({
-	     url   : execURL + "stock_monitoring_sel @field='"+field_name+"', @search='"+keyword+"'" 
-        ,width          : $(document).width() - 25
-	    ,height         : $(document).height() - 250
-	   // ,selectorType   : "checkbox"
-        ,blankRowsLimit :0
-       // ,isPaging : true
-        ,dataRows       :[
-     		 { text:"Part No."                      , width:150     , style:"text-align:left;"        
-     		    ,onRender: function(d){
-    		        return svn(d, "part_no");
-    		    }
-     		 }	 
-    		,{ text:"National Stock No."            , width:180     , style:"text-align:left;"    
-    		      ,onRender: function(d){
-    		        return svn(d, "national_stock_no");
-    		    }
-    		}
-    		
-    		,{ text:"Nomenclature Name"                          , width:300     , style:"text-align:left;"
-    		    ,onRender: function(d){
-    		        return svn(d, "item_name");
-    		    }
-    		}
-    		,{ text:"Unit of Measure"      , width:200     , style:"text-align:left;"
-    		    ,onRender: function(d){
-    		        return svn(d, "unit_of_measure");
-    		    }
-    		}
-    		,{ text:"Available Stocks"  , width:150     , style:"text-align:center;"
-    		    ,onRender: function(d){
-    		        return svn(d, "stock_qty");
-    		    }
-    		}
-    		,{ text:"Warehouse Location"  , width:200   , style:"text-align:left;"
-    		    ,onRender: function(d){
-    		        return svn(d, "warehouse_location");
-    		    }
-    		}
-    		,{ text:"Bin"  , width:100   , style:"text-align:left;"
-    		    ,onRender: function(d){
-    		        return svn(d, "bin");
-    		    }
-    		}
-	    ]
+        $("#grid").dataBind({
+    	     url   : execURL + "stock_monitoring_sel @field='"+ fieldFilter +"', @search='"+keywordFilter+"'" 
+            ,width          : $(document).width() - 25
+    	    ,height         : $(document).height() - 250
+            ,blankRowsLimit :0
+            ,dataRows       :[
+         		 { text:"Part No."                      , width:150     , style:"text-align:left;"        
+         		    ,onRender: function(d){
+        		        return svn(d, "part_no");
+        		    }
+         		 }	 
+        		,{ text:"National Stock No."            , width:180     , style:"text-align:left;"    
+        		      ,onRender: function(d){
+        		        return svn(d, "national_stock_no");
+        		    }
+        		}
+        		,{ text:"Nomenclature Name"                          , width:300     , style:"text-align:left;"
+        		    ,onRender: function(d){
+        		        return svn(d, "item_name");
+        		    }
+        		}
+                ,{text  : "Serviceable"                  , type  : "label"       , width : 100       , style : "text-align:center;" 
+                    ,onRender : function(d){ return svn(d,"stock_qty"); }
+                }
+        		,{text  : "For Repair"                  , type  : "label"       , width : 150       , style : "text-align:center;" 
+                    ,onRender : function(d){ return svn(d,"for_repair"); }
+        		}
+        		,{text  : "Total Stock Qty."         , type  : "label"       , width : 150       , style : "text-align:center;" 
+        		    ,onRender : function(d){ return svn(d,"ttl_stocks").toLocaleString("en"); }
+        		}
 
-	    ,onComplete: function(){
-	        $("#grid").removeClass("hide");
-        }
-    });   
+        		,{ text:"Unit of Measure"      , width:200     , style:"text-align:left;"
+        		    ,onRender: function(d){
+        		        return svn(d, "unit_of_measure");
+        		    }
+        		}
+        		,{ text:"Available Stocks"  , width:150     , style:"text-align:center;"
+        		    ,onRender: function(d){
+        		        return svn(d, "stock_qty");
+        		    }
+        		}
+        		,{ text:"Warehouse Location"  , width:200   , style:"text-align:left;"
+        		    ,onRender: function(d){
+        		        return svn(d, "warehouse_location");
+        		    }
+        		}
+        		,{ text:"Bin"  , width:100   , style:"text-align:left;"
+        		    ,onRender: function(d){
+        		        return svn(d, "bin");
+        		    }
+        		}
+    	    ]
+    	    ,onComplete: function(){
+    	        $("#grid").removeClass("hide");
+            }
+        });   
     }
-}       
+}         
