ff2c832b590e349eb4405c96e395942a6b6a62bf
German M. Fuentes <gm_fuentes@yahoo.com>
2017-08-16 03:52:13 +0800

    added user_id and system_pages

--- pafeis/dbWebSource/procedures_functions/app_profile_sel.sql
+++ pafeis/dbWebSource/procedures_functions/app_profile_sel.sql
@@ -1,4 +1,8 @@
-create procedure [dbo].[app_profile_sel]
+CREATE procedure [dbo].[app_profile_sel]
+(
+   @user_id INT = NULL
+)
 as
-select top 1 * from dbo.app_profile
-
+	select top 1 *
+		,'selectoption,masterpages,table,filemanager,tablelayout,errors,appprofile' system_pages 
+	from dbo.app_profile
