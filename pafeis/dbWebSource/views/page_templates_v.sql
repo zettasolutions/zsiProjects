ff454613b790aeec02d9085a45456f52a79462d2
German M. Fuentes <gm_fuentes@yahoo.com>
2017-10-14 05:03:43 +0800

    update query from left join to inner join

--- pafeis/dbWebSource/views/page_templates_v.sql
+++ pafeis/dbWebSource/views/page_templates_v.sql
@@ -1,4 +1,6 @@
-create view dbo.page_templates_v 
+CREATE view dbo.page_templates_v 
  as
- select a.*,b.page_name,b.page_title,dbo.getUser(a.created_by) as created_by_name,dbo.getUser(a.updated_by) as updated_by_name from dbo.page_templates a left join dbo.pages b on a.page_id=b.page_id 
+ select a.*,b.page_name,b.page_title,dbo.getUser(a.created_by) as created_by_name,dbo.getUser(a.updated_by) as updated_by_name 
+ from dbo.page_templates a inner join dbo.pages b on a.page_id=b.page_id 
 
+ 
