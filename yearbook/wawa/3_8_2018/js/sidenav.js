99fafcfafce57cb6ed3d338525484fa62f1a4789
Vincent L. Solon <32336555+metagrosssolon@users.noreply.github.com>
2018-03-08 18:22:22 +0800

    basin masayop ko ba sorry daan :(

---
+++ yearbook/wawa/3_8_2018/js/sidenav.js
@@ -0,0 +1,9 @@
+// SideNav Initialization
+        $(".button-collapse").sideNav();
+
+        var container = document.getElementById('slide-out');
+        Ps.initialize(container, {
+            wheelSpeed: 2,
+            wheelPropagation: d,
+            minScrollbarLength: 50
+        });
