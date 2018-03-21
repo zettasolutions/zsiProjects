1b1fd98005c03a6fc7af2737cd783b00b3c15063
smager <gm_fuentes@yahoo.com>
2018-03-21 04:41:56 -0400

    update table design

--- pafeis/dbWebSource/tables/oracle_compile_scripts.sql
+++ pafeis/dbWebSource/tables/oracle_compile_scripts.sql
@@ -1,4 +1,6 @@
 CREATE TABLE oracle_compile_scripts(
 developer	VARCHAR(10)	NOT NULL
 ,source	NVARCHAR(MAX)	NULL
-,server_user	VARCHAR(50)	NULL)
+,server_user	VARCHAR(50)	NULL
+,response	VARCHAR(MAX)	NULL
+,response_counter	INT	NULL)
