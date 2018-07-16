afb97a4b8dc275034078c5ee626b6847c9a87191
Romel Dizon <rdizon83@gmail.com>
2018-07-17 01:11:30 +0800

    created types page
    CTS Types

---
+++ cts/dbWebSource/js/types.js
@@ -0,0 +1,54 @@
+(function() {
+    var bs = zsi.bs.ctrl
+        ,svn = zsi.setValIfNull
+    ;
+    
+    displayRecords();
+    
+    function displayRecords(){   
+        var cb = bs({name:"cbFilter1",type:"checkbox"});
+        $("#grid").dataBind({
+    	     url            : procURL + "types_sel" 
+    	    ,width          : $("#main-content").width() - 40
+    	    ,height         : $("#main-content").height() - 150
+    	    ,selectorType   : "checkbox"
+            ,blankRowsLimit : 5
+            ,dataRows : [
+                    { text : cb , type : "hidden" , width : 25 , style : "text-align:left;"       
+            		    , onRender : function(d) {
+                    	    return bs({name:"type_id",type:"hidden",value: svn(d,"type_id")})
+                    	        + bs({name:"is_edited",type:"hidden",value: svn(d,"is_edited")})
+                                + (d !==null ? bs({name:"cb",type:"checkbox"}) : "" );
+                        }
+                    }
+                    ,{text  : "Category"        , name  : "category_id"         , type  : "select"   , width : 300    , style : "text-align:left;"}
+                    ,{text  : "Type"            , name  : "type_desc"           , type  : "input"    , width : 300    , style : "text-align:left;"}
+             		,{text  : "Is Active"       , name  : "is_active"           , type  : "yesno"    , width : 80     , style : "text-align:left;"  ,defaultValue:"Y"}
+    	    ]
+    	     ,onComplete: function(){
+    	         $("#cbFilter1").setCheckEvent("#grid input[name='cb']");
+    	         $("select[name='category_id']").dataBind("categories");
+            }  
+        });    
+    }
+    
+    $("#btnSave").click(function() {
+        $("#grid").jsonSubmit({
+            procedure: "types_upd"
+            , optionalItems: ["is_active"]
+            ,onComplete: function (data) {
+                $("#grid").clearGrid(); 
+                displayRecords();
+            }
+        });
+    });
+    
+    $("#btnDelete").click(function(){
+        zsi.form.deleteData({
+             code       : "ref-0004"
+            ,onComplete : function(data){
+                            displayRecords();
+                        }
+        });      
+    });
+})(); 
