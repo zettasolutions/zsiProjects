ba2051f9ab57c1e092bfc6447e7e207ff518db82
Romel Dizon <rdizon83@gmail.com>
2018-07-20 02:31:15 +0800

    added application name culomn and added prototype.toDate
    STS

--- cts/dbWebSource/js/subscription.js
+++ cts/dbWebSource/js/subscription.js
@@ -1,3 +1,13 @@
+ String.prototype.toDate = function () {
+                if(!isValidDate(this)) return "";
+                var _date=new Date( Date.parse(this) );
+                var m =  (_date.getMonth()+1) + "";
+                var d = _date.getDate() + "";
+                m = (m.length==1? "0"+m:m);
+                d = (d.length==1? "0" +d:d);
+                return  _date.getFullYear() + '-'  + m + '-' + d;
+};  
+ 
  zsi.ready(function(){
     displayRecords();
     if (gUser.is_admin === "Y") {
@@ -22,7 +32,7 @@
         var cId = $("#company_filter").val();
         var cb = bs({name:"cbFilter1",type:"checkbox"});
         $("#grid").dataBind({
-    	     url            : procURL + "subscribe_appl_sel @client_id=" + (cId ? cId : 0)  
+    	     url            : procURL + "subscriptions_sel @app_client_id=" + (cId ? cId : null)  
     	    ,width          : $("#main-content").width() - 40
     	    ,height         : $("#main-content").height() - 150
     	    ,selectorType   : "checkbox"
@@ -30,27 +40,42 @@
             ,dataRows : [
                     { text : cb , type : "hidden" , width : 25 , style : "text-align:left;"       
             		    , onRender : function(d) {
-                    	    return bs({name:"app_id",type:"hidden",value: svn(d,"app_id")})
-                    	        + bs({name:"client_id",type:"hidden",value: svn(d,"client_id")})
-                    	        + bs({name:"type_id",type:"hidden",value: svn(d,"type_id")})
-                                + (d !==null ? bs({name:"cb",type:"checkbox"}) : "" );
+                    	    return bs({name:"subscription_id",type:"hidden",value: svn(d,"subscription_id")})
+                    	        + bs({name:"is_edited",type:"hidden",value: svn(d,"is_edited")})
+                    	        + bs({name:"app_id",type:"hidden",value: svn(d,"app_id")})
+                    	        + (d !==null ? bs({name:"cb",type:"checkbox"}) : "" );
                         }
                     }
-                    ,{text  : "Application Name"        , name  : "app_name"    , type  : "input"    , width : 300    , style : "text-align:left;"}
-                    ,{text  : "Application Desc"        , name  : "app_desc"    , type  : "input"    , width : 300    , style : "text-align:left;"}
-             		,{text  : "Is Active?"              , name  : "is_active"   , type  : "yesno"    , width : 80     , style : "text-align:left;"  ,defaultValue:"Y"}
+                    ,{text  : "Application Name"    , width : 300    , style : "text-align:left;"
+                        ,onRender : function(d){ return svn(d,"app_name")}
+                    }
+                    ,{text  : "Subscription Date"       , type : "input"    , width : 150    , style : "text-align:left;"
+                        ,onRender : function(d){ 
+                            return "<input name='subscription_date' type='date' value='"  +  svn(d,"subscription_date").toDate() +"' class='form-control' style='padding:0'>";
+                            
+                        }
+                    }
+                    ,{text  : "No. of Months"           , name : "no_months"            , type : "input"     , width : 150    , style : "text-align:left;"}
+                    ,{text  : "Expiry Date"             , width : 150    , style : "text-align:left;"
+                         ,onRender : function(d){ return svn(d,"expiry_date").toDateFormat()}
+                    }
+             		,{text  : "Is Active?"              , name  : "is_active"           , type  : "yesno"    , width : 80     , style : "text-align:left;"  ,defaultValue:"Y"}
     	    ]
-    	     ,onComplete: function(){
+    	     ,onComplete: function(o){
+    	         _dRows = o.data.rows;
+    	         console.log(_dRows);
+    	         zsi.initDatePicker();
     	         $("#cbFilter1").setCheckEvent("#grid input[name='cb']");
-    	         $("select[name='category_id']").dataBind("categories");
-            }  
+    	         //$("#grid input[name='subscription_date']").val(_dRows[0].subscription_date);
+    	    }  
         });    
     }
 
     $("#btnSave").click(function() {
         $("#grid").jsonSubmit({
-            procedure: "types_upd"
-            , optionalItems: ["is_active"]
+            procedure: "subscriptions_upd"
+            ,optionalItems: ["is_active"]
+            ,notInclude: "#expiry_date"
             ,onComplete: function (data) {
                 $("#grid").clearGrid(); 
                 displayRecords();
@@ -58,14 +83,6 @@
         });
     });
 
-    $("#btnDelete").click(function(){
-        zsi.form.deleteData({
-             code       : "ref-0004"
-            ,onComplete : function(data){
-                            displayRecords();
-                        }
-        });      
-    });
 });    
-    
-   
+ 
+        
