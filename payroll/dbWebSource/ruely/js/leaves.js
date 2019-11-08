25f4f593c0aba888cbccb890b1f9e8a2b01b8d04
zettaRuel <54260103+zettaRuel@users.noreply.github.com>
2019-11-08 22:53:06 +0800

    levels, positions, level other income and employees (js,html) has been added/updated

--- payroll/dbWebSource/ruely/js/leaves.js
+++ payroll/dbWebSource/ruely/js/leaves.js
@@ -10,12 +10,12 @@
         
         $("#userId").dataBind({
              sqlCode     : "U77" //users_sel
-            ,text        : "last_name"
+            ,text        : ("first_name"," ","middle_name"," ","last_name")
             ,value       : "user_id"
         })
-         $("#leaveTypeId").dataBind({
+        $("#leaveTypeId").dataBind({
              sqlCode     : "L187" //leave_types_sel
-            ,text        : ("leave_type")
+            ,text        : "leave_type"
             ,value       : "leave_type_id"
         })
     };
@@ -23,16 +23,18 @@
     
     runDatePicker = function(){
         $('#datepicker').datepicker(
-            {
-                 todayHighlight  : true
-                ,multidate       : true
-                ,pickTime        : false
-                
-            })
+        {
+             todayHighlight  : true
+            ,multidate       : true
+            ,pickTime        : false
+            
+        }).on('changeDate', function(e) {
+            $(this).closest('.input-group').find('.count').text(' ' + e.dates.length);
+        });
             
     };
     
-     $("#btnSaveLeaves").click(function () {
+    $("#btnSaveLeaves").click(function () {
        $("#gridLeaves").jsonSubmit({
                  procedure  : "filed_leaves_upd"
                 ,optionalItems: ["is_active"]
@@ -46,40 +48,39 @@
         zsi.form.deleteData({
              code       : "ref-00014"
             ,onComplete : function(data){
-                    $("#gridLeaves").trigger("refresh");
-                  }
+                $("#gridLeaves").trigger("refresh");
+              }
         });       
     });
     
     $("#btnGo").click(function () {
         var _row  = $(this).closest(".row");
         var _date = _row.find('#datepicker').val();
-        var _userId = _row.find('#userId').val();
-        var _leaveTypeId = _row.find('#leaveTypeId').val();
+        var _user = _row.find('#userId').val();
+        var _leaveType = _row.find('#leaveTypeId').val();
         var _rows = [];
         var _info = null;
         
+        x = _date;
         
         $.each(_date.split(","), function(i,v){
             _info = { 
-                 employee_id    : _userId
-                ,leave_type_id  : _leaveTypeId
+                 employee_id    : _user
+                ,leave_type_id  : _leaveType
                 ,leave_date     : v
             };
             
-            _rows.push(_info);
-            
-            if(i > 0){
+            if(i >= 0 && _date !== ""){
                 $(".row").find("#gridId").removeClass("d-none");
                 $(".row").find("#btnId").removeClass("d-none");
             }else{
-               $(".row").find("#gridId").addClass("d-none");
-               $(".row").find("#btnId").addClass("d-none");
+                $(".row").find("#gridId").addClass("d-none");
+                $(".row").find("#btnId").addClass("d-none");
             }
+             _rows.push(_info);
         });
        
-            displayDateOfLeaves(_rows);
-      
+        displayDateOfLeaves(_rows);
 
     });
     
@@ -90,19 +91,19 @@
             ,width          : $(".zContainer").width() 
             ,height         : $(document).height("")
             ,dataRows       : [
-                {text: "File Date"                                                         ,width : 200   ,style : "text-align:left;"
+                {text: "File Date"                                                              ,width : 190   ,style : "text-align:left;"
                     ,onRender  :  function(d)
-                        { return   app.bs({name:"leave_id"                  ,type:"hidden"  ,value: app.svn(d,"leave_id")})
-                                 + app.bs({name:"is_edited"                 ,type:"hidden"  ,value: app.svn(d,"is_edited")}) 
-                                 + app.bs({name:"employee_id"               ,type:"hidden"  ,value: app.svn(d,"employee_id")})
-                                 + app.bs({name:"leave_type_id"             ,type:"hidden"  ,value: app.svn(d,"leave_type_id")})
-                                 + app.bs({name:"file_date"                 ,type:"input"   ,value: app.svn(d,"file_date")});
+                        { return   app.bs({name:"leave_id"                  ,type:"hidden"      ,value: app.svn(d,"leave_id")})
+                                 + app.bs({name:"is_edited"                 ,type:"hidden"      ,value: app.svn(d,"is_edited")}) 
+                                 + app.bs({name:"employee_id"               ,type:"hidden"      ,value: app.svn(d,"employee_id")})
+                                 + app.bs({name:"leave_type_id"             ,type:"hidden"      ,value: app.svn(d,"leave_type_id")})
+                                 + app.bs({name:"file_date"                 ,type:"input"       ,value: app.svn(d,"file_date")});
                         }
                 }
-                ,{text: "Leave Date"            ,name:"leave_date"          ,type:"input"       ,width : 200   ,style : "text-align:left;"}
-                ,{text: "Filed Hours"           ,name:"filed_hours"         ,type:"input"       ,width : 200   ,style : "text-align:left;"}
-                ,{text: "Approved Hours"        ,name:"approved_hours"      ,type:"input"       ,width : 200   ,style : "text-align:left;"}
-                ,{text: "Approved?"                                                             ,width : 70    ,style : "text-align:left;" ,defaultValue:"Y"
+                ,{text: "Leave Date"            ,name:"leave_date"          ,type:"input"       ,width : 190   ,style : "text-align:left;"}
+                ,{text: "Filed Hours"           ,name:"filed_hours"         ,type:"input"       ,width : 150   ,style : "text-align:left;"}
+                ,{text: "Approved Hours"        ,name:"approved_hours"      ,type:"input"       ,width : 150   ,style : "text-align:left;"}
+                ,{text: "Approved?"                                                             ,width : 70    ,style : "text-align:left;" ,defaultValue:"N"
                     ,onRender  :  function(d)
                         { return   app.bs({name:"is_approved"               ,type:"yesno"       ,value: app.svn(d,"is_approved")})
                                  + app.bs({name:"is_approved_by"            ,type:"hidden"      ,value: app.svn(d,"is_approved_by")})
@@ -112,9 +113,9 @@
                 ,{text: "Leave Reason"          ,name:"leave_reason"        ,type:"input"       ,width : 200   ,style : "text-align:left;"}
                 ,{text: "Approver Comment"                                                      ,width : 200   ,style : "text-align:left;"
                     ,onRender  :  function(d)
-                        { return   app.bs({name:"approver_comment"          ,type:"input"   ,value: app.svn(d,"approver_comment")})  
-                                 + app.bs({name:"created_by"                ,type:"hidden"  ,value: app.svn(d,"created_by")})
-                                 + app.bs({name:"created_date"              ,type:"hidden"  ,value: app.svn(d,"created_date")}) ;
+                        { return   app.bs({name:"approver_comment"          ,type:"input"       ,value: app.svn(d,"approver_comment")})  
+                                 + app.bs({name:"created_by"                ,type:"hidden"      ,value: app.svn(d,"created_by")})
+                                 + app.bs({name:"created_date"              ,type:"hidden"      ,value: app.svn(d,"created_date")}) ;
                         }
                 }
                 
@@ -126,4 +127,4 @@
             }
         });
     } 
-})();  
+})();     
