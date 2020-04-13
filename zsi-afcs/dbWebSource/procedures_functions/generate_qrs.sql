cee5f49fe67ff15cd99bd2bccfa5e3f0129ffc7e
Jaypee Mahilum <hlmahilum>
2020-04-14 05:20:07 +0800

    sql file 04-14-2020

--- zsi-afcs/dbWebSource/procedures_functions/generate_qrs.sql
+++ zsi-afcs/dbWebSource/procedures_functions/generate_qrs.sql
@@ -1,8 +1,8 @@
-create procedure generate_qrs 
+CREATE procedure [dbo].[generate_qrs] 
 as
 begin
 declare @ctr INT = 0
-  while @ctr < 1000
+  while @ctr < 100000
   BEGIN
      INSERT INTO dbo.generated_qrs (hash_key,is_taken, is_active, balance_amt, created_by, created_date) values (newid(), 'N', 'Y',0,3,GETDATE());
 	 SET @ctr = @ctr + 1;
