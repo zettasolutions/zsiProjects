233d4a9d558ab6550323b3013c3b4c3c8f59973d
MJDT <markjoseph.tadlip@gmail.com>
2020-05-13 23:28:14 +0800

    Update client  page

--- zsi-crm/dbWebSource/js/clients.js
+++ zsi-crm/dbWebSource/js/clients.js
@@ -1,51 +1,134 @@
  var clients = (function(){
-    var  _public            = {}
-        ,bs                 = zsi.bs.ctrl
-        ,svn                = zsi.setValIfNull 
+    var _public = {}
+        ,gMdlAddClient = "modalWindowAddClient"
+        ,gTW = null
     ;
+    
     zsi.ready = function(){
         $(".page-title").html("Clients");
-        $(".panel-container").css("min-height", $(window).height() - 160);
         
-        //$("#client_phone_no").inputmask({"mask": "(99) 9999 - 9999"});
-        
-        $('#country_id').select2({placeholder: "",allowClear: true});
-        $('#state_id').select2({placeholder: "",allowClear: true});
-        $('#city_id').select2({placeholder: "",allowClear: true});
-        $("#country_id").dataBind({
-            sqlCode      : "D247" //dd_countries_sel
-           ,text         : "country_name"
-           ,value        : "country_id"
-           ,onChange     : function(d){
-               var _info           = d.data[d.index - 1]
-                   country_id         = isUD(_info) ? "" : _info.country_id;
-               $("#state_id").dataBind({
-                    sqlCode      : "D248" //dd_states_sel
-                   ,parameters   : {country_id:country_id}
-                   ,text         : "state_name"
-                   ,value        : "state_id"
-                   ,onChange     : function(d){
-                       var _info           = d.data[d.index - 1]
-                           state_id         = isUD(_info) ? "" : _info.state_id;
-                           
-                       $("#city_id").dataBind({
-                            sqlCode      : "D246" //dd_cities_sel
-                           ,parameters   : {state_id:state_id}
-                           ,text         : "city_name"
-                           ,value        : "city_id"
-                           ,onChange     : function(d){
-                               var _info           = d.data[d.index - 1]
-                                   city_id         = isUD(_info) ? "" : _info.city_id;
-                               
-                           }
-                        });
-                       
-                   }
-                });
-           }
-        });
-        
+        gTW = new zsi.easyJsTemplateWriter();
+        getTemplates();
+        displayClients();
     };
+    
+    function getTemplates(){
+        new zsi.easyJsTemplateWriter($("#generatedComponents").empty())
+        .bsModalBox({
+              id        : gMdlAddClient
+            , sizeAttr  : "modal-full"
+            , title     : "New Client"
+            , body      : gTW.new().modalBodyClient({onClickSaveClient:"submitNewClient();"}).html()
+        });
+    }
+    
+    function displayClients(){
+        $("#gridClients").dataBind({
+             sqlCode     : "C241" //clients_sel
+            ,height      : 400
+            ,dataRows    : [
+                {text: "Code", width: 130, style: "text-align:center"
+                    ,onRender : function(d){ 
+                        return app.svn (d,"client_code");
+                    }
+                }
+                ,{text: "Name", width : 250, style: "text-align:left"
+                    ,onRender : function(d){ 
+                        return app.svn (d,"client_name");
+                    }
+                }
+                ,{text: "Phone No.", width: 120, style: "text-align:left"
+                    ,onRender  :  function(d){ 
+                        return app.svn (d,"client_phone_no");
+                    }
+                }
+                ,{text: "Mobile No.", width: 120, style: "text-align:left"
+                    ,onRender  :  function(d){ 
+                        return app.svn (d,"client_mobile_no");
+                    }
+                }
+                ,{text: "Email Address", width: 200, style: "text-align:left"
+                    ,onRender  :  function(d){ 
+                        return app.svn (d,"client_email_add");
+                    }
+                }
+                ,{text: "Billing Address", width: 300, style: "text-align:left"
+                    ,onRender  :  function(d){ 
+                        return app.svn (d,"billing_address");
+                    }
+                }
+            ]
+            ,onComplete  : function(o){
+                
+            }
+        });
+    }
+
+    function validations(){
+        var forms = document.getElementsByClassName('needs-validation');
+    	// Loop over them and prevent submission
+    	var validation = Array.prototype.filter.call(forms, function(form) {
+    		form.addEventListener('submit', function(event) {
+    			if (form.checkValidity() === false) {
+    				event.preventDefault();
+    				event.stopPropagation();
+    			    $("form").addClass('was-validated');
+    			}else{
+        			event.preventDefault();
+        			event.stopPropagation();
+    			    $('#myModal').modal('show');
+    			    $("form").addClass('was-validated');
+    			}
+    		}, false);
+    	});
+    }
+    
+    $("#btnNew").click(function() {
+        var _$mdl = $("#" + gMdlAddClient);
+        var _$frm = _$mdl.find("form");
+        var _$country = _$mdl.find('#country_id')
+            ,_$state = _$mdl.find('#state_id')
+            ,_$city = _$mdl.find('#city_id');
+        _$mdl.find(".modal-title").text("New Client") ;
+        _$mdl.modal({ show: true, keyboard: false, backdrop: 'static' });
+        _$mdl.find(".modal-footer").addClass("justify-content-start");
+        
+        validations();
+        _$country.select2({placeholder: "",allowClear: true, dropdownParent: _$mdl});
+        _$state.select2({placeholder: "",allowClear: true, dropdownParent: _$mdl});
+        _$city.select2({placeholder: "",allowClear: true, dropdownParent: _$mdl});
+        _$country.dataBind({
+            sqlCode : "D247" //dd_countries_sel
+            ,text : "country_name"
+            ,value : "country_id"
+            ,onChange : function(d){
+                var _info = d.data[d.index - 1]
+                    ,country_id = isUD(_info) ? "" : _info.country_id;
+                
+                _$state.dataBind({
+                    sqlCode : "D248" //dd_states_sel
+                    ,parameters : {country_id:country_id}
+                    ,text : "state_name"
+                    ,value : "state_id"
+                    ,onChange : function(d){
+                        var _info = d.data[d.index - 1]
+                            ,state_id = isUD(_info) ? "" : _info.state_id;
+                           
+                        _$city.dataBind({
+                            sqlCode      : "D246" //dd_cities_sel
+                            ,parameters   : {state_id:state_id}
+                            ,text         : "city_name"
+                            ,value        : "city_id"
+                            ,onChange     : function(d){
+                                var _info = d.data[d.index - 1]
+                                    ,city_id = isUD(_info) ? "" : _info.city_id;
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
     
     $("#btnSaveClient").click(function () {
         $("#formClients").jsonSubmit({
@@ -70,31 +153,5 @@
         }); 
     });
     
-
-  window.addEventListener('load', function() {
-	// Fetch all the forms we want to apply custom Bootstrap validation styles to
-	var forms = document.getElementsByClassName('needs-validation');
-	// Loop over them and prevent submission
-	var validation = Array.prototype.filter.call(forms, function(form) {
-		form.addEventListener('submit', function(event) {
-			if (form.checkValidity() === false) {
-				event.preventDefault();
-				event.stopPropagation();
-			    $("form").addClass('was-validated');
-			}else{
-    			event.preventDefault();
-    			event.stopPropagation();
-			    $('#myModal').modal('show');
-			    $("form").addClass('was-validated');
-			}
-		}, false);
-	});
-  }, false);
-
-   
-    
     return _public;
-    
-    
-    
-})();   
+})();      
