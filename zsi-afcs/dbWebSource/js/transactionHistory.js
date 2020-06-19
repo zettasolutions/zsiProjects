dbb2479a48d5c08890522e171e40e4f8d274ef22
rechiearnado21 <54617842+rechiearnado21@users.noreply.github.com>
2020-06-19 11:40:18 +0800

    CHANGES IN validation

--- zsi-afcs/dbWebSource/js/transactionHistory.js
+++ zsi-afcs/dbWebSource/js/transactionHistory.js
@@ -1,11 +1,7 @@
  var th = (function(){ 
     var _pub                = {}
         ,bs                 = zsi.bs.ctrl
-        ,gtw                = null
-        ,mdlAddNewUser      = "modalWindowAddNewUser"
-        ,gMdlUploadExcel    = "modalWindowUploadExcel"
-        ,gCompanyId         = app.userInfo.company_id
-        ,mdlImageUser       = "modalWindowImageUser" 
+        ,gtw                = null 
         ,gVLName            = ""
         ,gVfName            = ""
         ,gVfrom             = ""
@@ -15,7 +11,22 @@
         ,gTravelDate        = ""
     ;
     zsi.ready = function(){
-        $(".page-title").html("Transaction History"); 
+        $(".page-title").html("Transaction History");   
+        validation();
+    }; 
+    
+    _pub.showModalTransaction = function (eL,id,vehiclePlateNo,fileName,Fname,Pdate) { 
+        var _frm = $("#frm_modalTransactionHistory"); 
+        var _imgFilename = fileName !=="" ? "/file/viewImage?fileName="+fileName : "../img/avatar-m.png"; 
+        _frm.find("#plateNoId").text(vehiclePlateNo); 
+        _frm.find("#imgFilename").attr("src", _imgFilename); 
+        _frm.find("#passenger_id").text(Fname); 
+        _frm.find("#payment_date").text(Pdate.toShortDate()); 
+        $('#modalTransactionHistory').modal({ show: true, keyboard: false, backdrop: 'static' }); 
+        
+    };  
+    
+    function validation(){
         $("#passengerRadio").click(function(){
             $("#passengerForm").removeClass("hide");
             $("#vehicleForm").addClass("hide");
@@ -28,46 +39,29 @@
             $("#PassengerRow").addClass("hide");
             $("#vehicleForm").find("input").val("");
         });
+    /*                  DATEPICKER VALIDATION                                  */  
         $("#travel_date").datepicker({
              autoclose : true
             ,todayHighlight: true 
         }).datepicker("setDate", "0");  
-        validation();
-    };
-    
-    _pub.showModalTransaction = function (eL,id,vehiclePlateNo,fileName,Fname,Pdate) { 
-        var _frm = $("#frm_modalTransactionHistory"); 
-        var _imgFilename = fileName !=="" ? "/file/viewImage?fileName="+fileName : "../img/avatar-m.png"; 
-        _frm.find("#plateNoId").text(vehiclePlateNo); 
-        _frm.find("#imgFilename").attr("src", _imgFilename); 
-        _frm.find("#passenger_id").text(Fname); 
-        _frm.find("#payment_date").text(Pdate.toShortDate()); 
-        $('#modalTransactionHistory').modal({ show: true, keyboard: false, backdrop: 'static' }); 
-        
-    }; 
-    function validation(){ 
-       $("#V_toDate").attr("readonly",true);
-        $("#btnAppend").attr("readonly",true); 
+        $("#V_toDate").attr("disabled",true);
         $("#V_fromDate").datepicker({
              autoclose : true
             ,todayHighlight: true 
-        }).on("changeDate",function(e){ 
-            gVfrom = $(this).val();
-            $("#V_toDate").removeAttr("readonly",true);
-            $("#btnAppend").removeAttr("readonly",true);
+        }).on("changeDate keyup",function(e){ 
+            if($(this).val() ===""){ $("#V_toDate").attr("disabled",true); $("#V_toDate").val(""); }
+            else{ $("#V_toDate").removeAttr("disabled"); } 
+            gVfrom = $(this).val(); 
+            $("#V_toDate").removeAttr("readonly",true); 
             $("#V_toDate").datepicker('setStartDate', e.date).on("change",function(){
                gVtoDate = $(this).val();
             }); 
         }); 
-        $("#V_last_name,#V_first_name").on("keyup change",function(){
-            gVLName = $.trim($(this).val());
-            gVfName = $.trim($(this).val());    
-        });
-         
-    } 
+     /*                  DATEPICKER VALIDATION                                  */  
+        
+    }
     function displayPassengerHistory(){
-        var cb = app.bs({name:"cbFilter1",type:"checkbox"});
-        ctr=-1;
+        var cb = app.bs({name:"cbFilter1",type:"checkbox"}); 
         $("#gridPassengerHistory").dataBind({
              sqlCode        : "P1295" //passenger_travel_history_sel 
              ,parameters     : {
@@ -84,30 +78,24 @@
                             var _link = "<a href='javascript:void(0)' ' title='View Transactions'onclick='th.showModalTransaction(this,"+ app.svn (d,"consumer_id") +", \""+ app.svn (d,"vehicle_plate_no") +"\",\""+ app.svn (d,"vehicle_img_filename") +"\",\""+ app.svn (d,"full_name") +"\",\""+ app.svn (d,"payment_date") +"\")'><i class='fas fa-eye'></i></a>";
                             return (d !== null ? _link : "");
                     }
-                }
-                 
-            ]
-            ,onPageChange : function(){
-                ctr=-1; 
-            }
+                } 
+            ] 
             ,onComplete: function(o){
                 gUsersData = o.data.rows;
-                $('.has-tooltip').tooltip(); 
-            
-                
+                $('.has-tooltip').tooltip();  
             }
         });
         
     }   
-    function displayVehicleHistory(){ 
+    function displayVehicleHistory(){  
         var cb = app.bs({name:"cbFilter1",type:"checkbox"}); 
         $("#gridVehicleHistory").dataBind({
              sqlCode        : "V1296" //vehicle_travel_history_sel 
-             ,parameters     : {
-                                 last_name      :  (gVLName ? gVLName : '')
-                                ,first_name     :  (gVfName ? gVfName : '')
-                                ,time_start     : (gVfrom ? gVfrom : '')
-                                ,time_end       : (gVtoDate ? gVtoDate : '')
+             ,parameters    : {
+                                 last_name      :  (gVLName  ? gVLName  : '')
+                                ,first_name     :  (gVfName  ? gVfName  : '')
+                                ,time_start     :  (gVfrom   ? gVfrom   : '')
+                                ,time_end       :  (gVtoDate ? gVtoDate : '')
             }
     	    ,height         : $(window).height() - 300  
             ,dataRows       : [
@@ -120,9 +108,9 @@
                 gUsersData = o.data.rows;
                 $('.has-tooltip').tooltip();  
             }
-        });
-        
+        }); 
     }
+    
     /*Validation for Passenger*/
     $("#btnPassenger").click( function() {
         gPLName = $.trim($("#P_last_name").val());
@@ -133,20 +121,20 @@
     });
     $("#btnClearPassenger").click( function() {
         $("#passengerForm").find("input").val("");
+        displayPassengerHistory(""); 
     });
     
     /*Validation for Vehicle*/
     $("#btnClearVehicle").click( function() {
         $("#vehicleForm").find("input").val("");
-        $("#VehicleRow").addClass("hide"); 
-    });  
-    $("#btnVehicle").click( function() {  
+        displayVehicleHistory();
+         
+    });   
+    $("#btnSearchVehicle").click( function() {  
+        gVLName = $.trim($("#V_last_name").val());
+        gVfName = $.trim($("#V_first_name").val());  
         displayVehicleHistory();
         $("#VehicleRow").removeClass("hide"); 
-    });
-    $("#btnAppend").click(function(){
-        displayVehicleHistory();  
-        $("#VehicleRow").removeClass("hide");
-    });
+    }); 
     return _pub;
-})();                                    
+})();                                     
