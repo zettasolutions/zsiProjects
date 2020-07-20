2d2d336e2c5fbd4225f1805349e9f9687d62579e
smager <gm_fuentes@yahoo.com>
2020-07-20 09:29:52 -0400

    fix sql command pop up, revise code

--- zsi-afcs/dbWebSource/js/sqlcommands.js
+++ zsi-afcs/dbWebSource/js/sqlcommands.js
@@ -1,128 +1,133 @@
-(function(){
-
-   var sqlCommands = {
-     gMdlId         : "modalAdd"
-    ,editor         : null
-    ,_tw            :  zsi.easyJsTemplateWriter
-    ,$modal         : null
-    ,$is_procedure  : null
-    ,$id            : null
-    ,$code          : null
-    ,$is_public     : null
-    ,modalTemplate : function(){
-        return     { 
-          id    : this.gMdlId
-        , title : 'Add SQL Command'
-        , body : new this._tw().sqlCmdMdlBody().html()
-        , footer: '<button id="btnSave" type="button" class="btn btn-primary">Save</button><button id="btnRun" type="button" class="btn btn-primary">Run</button>' 
-    
-        };
-    } 
-    ,getTemplate  : function(callBack) {
-                    $("#"+ this.gMdlId).remove();
-                    $("body").append( new this._tw().bsModalBox( this.modalTemplate() ).html() );
-                    $modal = $("#" + this.gMdlId);
-                    $is_procedure = $modal.find('#is_procedure');
-                    $id = $modal.find('#sqlcmd_id');
-                    $code = $modal.find('#sqlcmd_code');
-                    $is_public = $modal.find('#is_public');
-                    $modal.find("form").append('<pre id="editor"></pre>');
-                    if (typeof callBack === "function") callBack();
-                }
-    ,initAceEditor      : function(readonly) {
-        var options = {
-            enableBasicAutocompletion: true,
-            enableSnippets: true,
-            enableLiveAutocompletion: false,
-            maxLines: Infinity,
-            fontSize: "10pt"
-        };
-        editor=null;
-        ace.require("ace/ext/language_tools");
-        editor = ace.edit("editor");
-        editor.setTheme("ace/theme/monokai");
-        editor.session.setMode("ace/mode/sql");
-        editor.setAutoScrollEditorIntoView(true);
+var sc = (function(){
+   var _pub = {
+         gMdlId         : "modalAdd"
+        ,editor         : null
+        ,tw             : {}
+        ,$modal         : null
+        ,$is_procedure  : null
+        ,$id            : null
+        ,$code          : null
+        ,$is_public     : null
+        ,data           : {}
+        ,modalTemplate : function(){
+            return     { 
+              id    : this.gMdlId
+            , title : 'Add SQL Command'
+            , body :  this.tw.sqlCmdMdlBody().html()
+            , footer: '<button id="btnSave" type="button" class="btn btn-primary">Save</button><button id="btnRun" type="button" class="btn btn-primary">Run</button>' 
         
-        if (readonly) {
-            options.readOnly = true;
-            options.highlightActiveLine = false;
-            options.highlightGutterLine = false;
-            editor.renderer.$cursorLayer.element.style.opacity = 0;
-        }
-    
-        editor.setOptions(options);
-    }
-    ,displaySqlCommands : function(callBack) {
-        $("#grid-SqlCommands").dataBind({
-             sqlCode:"S53"
-            ,parameters : { sort_index: 3 }
-            ,width      : $(".zContainer").width() 
-            ,height     : $(window).height() - 240
-            ,isPaging   : true
-            ,rowsPerPage : 100
-            ,dataRows : [
-                            { text : "Code" , width : 100 , style : "text-align:center;"
-                                ,onRender: function(d) {
-                                    return app.bs({ type : 'hidden' , name : 'sqlcmd_id' })
-                                        +  app.bs({ type : 'input' , name : 'sqlcmd_code' , value : app.svn(d,'sqlcmd_code') });
-                                }
-                            }
-                            ,{ text : "Text"            ,type:"input"       ,width : 300    ,style : "text-align:left;"     ,name : 'sqlcmd_text'  }
-                            ,{ text : "Is Proc?"        ,type:"input"       ,width : 80     ,style : "text-align:left;"     ,name : 'is_procedure' }
-                            ,{ text : "Is Public?"      ,type:"input"       ,width : 80     ,style : "text-align:left;"     ,name : 'is_public' }
-                            ,{ text : "Action" , width : 100 , style : "text-align:center;"
-                                ,onRender : function(d) {
-                                    return '<a href="javascript:void(0);" class="edit-sqlcommand"><i class="fas fa-edit"></i></a>';
-                                }
-                            }
-                        ]
-            ,onComplete : function(d) {
-                this
-                .off('click','.edit-sqlcommand')
-                .on('click','.edit-sqlcommand', function() {
-                   
-                    var _rowInfo = d.data.rows[$(this).closest('.zRow').index()];
-                    
-                    sqlCommands.getTemplate(function() {
-                        $modal.modal('show');
-                        
-                        $is_procedure.val(_rowInfo.is_procedure);
-                        $id.val(_rowInfo.sqlcmd_id);
-                        $code.val(_rowInfo.sqlcmd_code);
-                        $is_public.val(_rowInfo.is_public);
-                        
-                        $modal.find("#editor").empty();
-                        $modal.find("#editor").html($('<div/>').text(_rowInfo.sqlcmd_text).html());
-                        
-                        sqlCommands.initAceEditor((_rowInfo.is_procedure === 'N' ? false : true));
-                    });
-                });
-                if(callBack) callBack();
+            };
+        } 
+        ,getTemplate  : function(callBack) {
+
+                        $("#"+ this.gMdlId).remove();
+                        $("body").append( this.tw.bsModalBox( this.modalTemplate() ).html() );
+                        $modal = $("#" + this.gMdlId);
+                        $is_procedure = $modal.find('#is_procedure');
+                        $id = $modal.find('#sqlcmd_id');
+                        $code = $modal.find('#sqlcmd_code');
+                        $is_public = $modal.find('#is_public');
+                        $modal.find("form").append('<pre id="editor"></pre>');
+                        if (typeof callBack === "function") callBack();
+                    }
+        ,initAceEditor      : function(readonly) {
+            var options = {
+                enableBasicAutocompletion: true,
+                enableSnippets: true,
+                enableLiveAutocompletion: false,
+                maxLines: Infinity,
+                fontSize: "10pt"
+            };
+            editor=null;
+            ace.require("ace/ext/language_tools");
+            editor = ace.edit("editor");
+            editor.setTheme("ace/theme/monokai");
+            editor.session.setMode("ace/mode/sql");
+            editor.setAutoScrollEditorIntoView(true);
+            
+            if (readonly) {
+                options.readOnly = true;
+                options.highlightActiveLine = false;
+                options.highlightGutterLine = false;
+                editor.renderer.$cursorLayer.element.style.opacity = 0;
             }
-        });
-    }
-    ,saveSqlCommand     : function() {
-        var _sqlcmd_id = $id.val();
-        var _sqlcmd_code = $code.val();
-        var _sqlcmd_text = editor.getValue();
-        var _is_public = $is_public.val();
         
-        if (_sqlcmd_code === "") { alert("Please enter code"); return }
-        if (_sqlcmd_text === "") { alert("Please enter sql statement."); return }
-        
-        $.post(app.procURL + "sql_commands_upd @sqlcmd_id="+_sqlcmd_id+",@sqlcmd_code='"+_sqlcmd_code+"',@sqlcmd_text='"+_sqlcmd_text+",@is_public='"+_is_public+"'", function(d) {
-            zsi.form.showAlert("savedWindow");
-            displaySqlCommands();
-        });
-    }
+            editor.setOptions(options);
+        }
+        ,displaySqlCommands : function(callBack) {
+            $("#grid-SqlCommands").dataBind({
+                 sqlCode:"S53"
+                ,parameters : { sort_index: 3 }
+                ,width      : $(".zContainer").width() 
+                ,height     : $(window).height() - 240
+                ,isPaging   : true
+                ,rowsPerPage : 100
+                ,dataRows : [
+                                { text : "Code" , width : 100 , style : "text-align:center;"
+                                    ,onRender: function(d) {
+                                        return app.bs({ type : 'hidden' , name : 'sqlcmd_id' })
+                                            +  app.bs({ type : 'input' , name : 'sqlcmd_code' , value : app.svn(d,'sqlcmd_code') });
+                                    }
+                                }
+                                ,{ text : "Text"            ,type:"input"       ,width : 300    ,style : "text-align:left;"     ,name : 'sqlcmd_text'  }
+                                ,{ text : "Is Proc?"        ,type:"input"       ,width : 80     ,style : "text-align:left;"     ,name : 'is_procedure' }
+                                ,{ text : "Is Public?"      ,type:"input"       ,width : 80     ,style : "text-align:left;"     ,name : 'is_public' }
+                                ,{ text : "Action" , width : 100 , style : "text-align:center;"
+                                    ,onRender : function(d) {
+                                        return '<a href="javascript:void(0);" class="edit-sqlcommand" onclick="sc.onClickEdit(this);" ><i class="fas fa-edit"></i></a>';
+                                    }
+                                }
+                            ]
+                ,onComplete : function(d) {
+                    sc.data=d.data.rows;
+                    if(callBack) callBack();
+                }
+            });
+        }
+        ,saveSqlCommand     : function() {
+            var _sqlcmd_id = $id.val();
+            var _sqlcmd_code = $code.val();
+            var _sqlcmd_text = editor.getValue();
+            var _is_public = $is_public.val();
+            
+            if (_sqlcmd_code === "") { alert("Please enter code"); return }
+            if (_sqlcmd_text === "") { alert("Please enter sql statement."); return }
+            
+            $.post(app.procURL + "sql_commands_upd @sqlcmd_id="+_sqlcmd_id+",@sqlcmd_code='"+_sqlcmd_code+"',@sqlcmd_text='"+_sqlcmd_text + "',@is_public='"+_is_public+"'", function(d) {
+                zsi.form.showAlert("savedWindow");
+                sc.displaySqlCommands();
+            });
+        }
+        ,onClickEdit        : function(anchor){
+           var _rowInfo = sc.data[$(anchor).closest('.zRow').index()];
+            this.getTemplate(function() {
+                    $modal.modal('show');
+                    $is_procedure.val(_rowInfo.is_procedure);
+                    $id.val(_rowInfo.sqlcmd_id);
+                    $code.val(_rowInfo.sqlcmd_code);
+                    $is_public.val(_rowInfo.is_public);
+                    $modal.find("#editor").empty();
+                    $modal.find("#editor").html($('<div/>').text(_rowInfo.sqlcmd_text).html());
+                    
+                    setTimeout(function(){ 
+                        //sc.initAceEditor(false);
+                        sc.initAceEditor((_rowInfo.is_procedure === 'N' ? false : true));
+                    }, 100);
 
-};     
+            });            
+
+        }
+    }
+    ;     
+
+return _pub;
+
+})();
 
 
 zsi.ready = function() {
     $(".page-title").html("SQL Commands");
-    var sc = Object.create(sqlCommands);
+    sc.tw = new zsi.easyJsTemplateWriter();
     sc.displaySqlCommands();
 
     $("#btnSync").click(function() {
@@ -181,9 +186,7 @@ zsi.ready = function() {
             }
         );
     });
-    
     $(document).on("click", "#btnSave", sc.saveSqlCommand);
-    
     $(window).bind('keydown', function (e) {
         var isCtrlS = (e.ctrlKey && e.which == 83);
         if (isCtrlS) {
@@ -195,7 +198,4 @@ zsi.ready = function() {
     
 };  
 
-    
-})();
-
-          
+                
